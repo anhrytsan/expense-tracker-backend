127219776a23a995d4aa219b10744d473db86fdf
Andriian <an.hrytsan@gmail.com>
2025-09-07 17:39:31 +0300

    allowed vercel server

--- server.js
+++ server.js
@@ -16,7 +16,24 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const { DB_HOST } = process.env;
 
-app.use(cors());
+const allowedOrigins = [
+  'https://expense-tracker-frontend-rho-nine.vercel.app',
+  'http://localhost:4200'
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const swaggerOptions = {
